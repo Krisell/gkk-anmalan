dbd6a5221f2aa7faf9839214f6e8f80f6849c9d3
Martin Krisell <martin.krisell@gmail.com>
2020-01-02 19:26:50 +0100

    Extract navigation to vue component

---
+++ resources/js/components/Navigation.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="actions">
+    <action-card description="Tävlingsanmälan för GKK-aktiv" icon="trophy"></action-card>
+    <action-card description="Funktionärsanmälan för GKK-aktiva" icon="users"></action-card>
+    <action-card description="Intresseanmälan från andra föreningar att tävla med GKK" icon="lightbulb-o"></action-card>
+    <action-card @click="admin" description="Admin" icon="lock"></action-card>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    admin () {
+      console.log("ADMIN")
+      window.location = '/admin'
+    }
+  }
+}
+</script>
+
+<style>
+.actions {
+    display: flex;
+    margin: 30px;
+    flex-direction: row;
+}
+
+.actions div {
+    margin: 10px;
+}
+
+@media (max-width: 1100px) {
+  .actions {
+    flex-direction: column;
+  }
+}
+
+</style>
