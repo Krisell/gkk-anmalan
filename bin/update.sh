25a8e4a9b8f5ac7dd2f014e6ad863c8a1152eab1
Martin Krisell <martin.krisell@gmail.com>
2020-01-21 16:03:24 +0100

    Add config and route cache to deploy script

--- bin/update.sh
+++ bin/update.sh
@@ -5,5 +5,7 @@ git pull
 cp /customers/d/f/2/gkk-styrkelyft.se/httpd.private/anmalan/public/css/* /customers/d/f/2/gkk-styrkelyft.se/httpd.www/anmalan/css
 cp /customers/d/f/2/gkk-styrkelyft.se/httpd.private/anmalan/public/js/* /customers/d/f/2/gkk-styrkelyft.se/httpd.www/anmalan/js
 cp /customers/d/f/2/gkk-styrkelyft.se/httpd.private/anmalan/public/mix-manifest.json /customers/d/f/2/gkk-styrkelyft.se/httpd.www/anmalan/mix-manifest.json
+php /customers/d/f/2/gkk-styrkelyft.se/httpd.private/anmalan/artisan config:cache
+php /customers/d/f/2/gkk-styrkelyft.se/httpd.private/anmalan/artisan route:cache
 
 echo Assets updated!
