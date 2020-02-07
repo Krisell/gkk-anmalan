24496051409cb174b2b2583db23f05b2482bb3fd
Martin Krisell <martin.krisell@gmail.com>
2020-02-07 10:30:24 +0100

    Update email text

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # Välkommen till GKKs anmälningssidor
 
-Du hittar dessa på anmalan.gkk-styrkelyft.se och funktionaliteten kommer byggas på under kommande månader.
+Du hittar dessa på https://anmalan.gkk-styrkelyft.se och funktionaliteten kommer byggas på framöver.
 
 Hälsningar,<br>
 {{ config('app.name') }}
