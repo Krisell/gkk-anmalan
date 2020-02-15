b810ce3777ff123b49abda6351077690b4729b72
Martin Krisell <martin.krisell@gmail.com>
2020-02-15 15:02:26 +0100

    Add link to go to start page

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -7,14 +7,22 @@
               GKK
             </div>
           </a>
+
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
           </button>
 
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav mr-auto"></ul>
-
               <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                  <a class="nav-link" href="/" style="display: flex; align-items: center;">
+                    <div style="margin-right: 5px;">Start</div>
+                    <!-- <img style="height: 30px; margin-right: 5px;" src="https://www.gkk-styrkelyft.se/wp-content/uploads/2014/08/Tv%c3%a5f%c3%a4rg-p%c3%a5-m%c3%b6rk-bakgrund-transparent.png"> -->
+                  </a>
+                </li>
+              </ul>
+
+              <ul class="navbar-nav">
                 <template v-if="!user">
                   <li class="nav-item"><a class="nav-link" href="/login">Logga in</a></li>
                   <li class="nav-item"><a class="nav-link" href="/register">Skapa konto</a></li>
