48a9a24adb1fc004d3a7557f30592df8c952cd82
Martin Krisell <martin.krisell@gmail.com>
2020-09-29 21:47:45 +0200

    Add Document model and fetch all stored documents to view

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Document;
 use Illuminate\Http\Request;
 
 class DocumentController extends Controller
@@ -9,13 +10,7 @@ class DocumentController extends Controller
     public function index()
     {
         return view('documents', [
-            'documents' => [
-                // These access token links have been revoked. Read from local file instead?
-                // ['name' => 'Protokoll 1, 2020-03-08', 'url' => 'https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/protokoll%2F2020%2FStyrelseprotokoll%201.%2020200308.pdf?alt=media&token=e6b803ee-5c82-42bf-8f0c-726a4c871b2b'],
-                // ['name' => 'Protokoll 2, 2020-03-15', 'url' => 'https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/protokoll%2F2020%2FStyrelseprotokoll%202.%2020200315.pdf?alt=media&token=b13e7d66-775e-40b7-bab6-d910ed840fec'],
-                // ['name' => 'Protokoll 3, 2020-04-19', 'url' => 'https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/protokoll%2F2020%2FStyrelseprotokoll%203.%2020200419.pdf?alt=media&token=223c5a71-55b6-477d-bc4e-b78d7353b7d9'],
-                // ['name' => 'Protokoll 4, 2020-06-08', 'url' => 'https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/protokoll%2F2020%2FStyrelseprotokoll%204%2020200608.pdf?alt=media&token=bb4bf750-8aa5-417b-8576-9425ba2c0f54'],
-            ],
+            'documents' => Document::all(),
         ]);
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\User;
+use App\Document;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -35,6 +36,11 @@ class DatabaseSeeder extends Seeder
             'email' => 'martinnormal@gmail.com',
             'password' => Hash::make('asdasd'),
             'role' => null,
+        ]);
+
+        Document::create([
+            'name' => 'NNR',
+            'url' => 'https://www.livsmedelsverket.se/globalassets/publikationsdatabas/andra-sprak/nordic-nutrition-recommendations-2012.pdf?AspxAutoDetectCookieSupport=1',
         ]);
     }
 }
