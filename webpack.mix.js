28da4ef0fab50d59e5f99f1048dcda18b3e7fe0f
Martin Krisell <martin.krisell@gmail.com>
2021-02-24 20:21:48 +0100

    Update npm dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,8 +5,12 @@ mix
     output: {
       chunkFilename: 'js/[name].js?id=[chunkhash]',
     },
+    resolve: {
+      fallback: { stream: false },
+    },
   })
   .options({
+    legacyNodePolyfills: true,
     processCssUrls: false,
     terser: {
       extractComments: false,
@@ -15,3 +19,4 @@ mix
   .js('resources/js/app.js', 'public/js')
   .postCss('resources/css/main.css', 'public/css', [require('tailwindcss')])
   .version()
+  .vue()
