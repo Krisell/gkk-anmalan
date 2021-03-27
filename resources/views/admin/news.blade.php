ee217208ced72f59989fb87fa4a0ab1e6aad0aa0
Martin Krisell <martin.krisell@gmail.com>
2021-03-27 11:01:40 +0100

    Update Trix for news editing

--- resources/views/admin/news.blade.php
+++ resources/views/admin/news.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
 @section('head')
-<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/trix@1.2.3/dist/trix.css">
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/trix@1.2.3/dist/trix.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/trix@1.3.1/dist/trix.css">
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/trix@1.3.1/dist/trix.js"></script>
 @endsection
 
 @section('content')
