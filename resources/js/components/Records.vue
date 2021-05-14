5681e1e1569bb56dfcc28dd531260046e0682b7e
Martin Krisell <martin.krisell@gmail.com>
2021-05-14 16:06:01 +0200

    Auto-format html

--- resources/js/components/Records.vue
+++ resources/js/components/Records.vue
@@ -13,27 +13,82 @@
               <thead>
                 <tr>
                   <th
-                    class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-center text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                    class="
+                      px-6
+                      py-3
+                      border-b border-gray-200
+                      bg-gray-50
+                      text-center text-xs
+                      leading-4
+                      font-medium
+                      text-gray-500
+                      uppercase
+                      tracking-wider
+                    "
                   >
                     Klass
                   </th>
                   <th
-                    class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-center text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                    class="
+                      px-6
+                      py-3
+                      border-b border-gray-200
+                      bg-gray-50
+                      text-center text-xs
+                      leading-4
+                      font-medium
+                      text-gray-500
+                      uppercase
+                      tracking-wider
+                    "
                   >
                     Gren (KSL)
                   </th>
                   <th
-                    class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-center text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                    class="
+                      px-6
+                      py-3
+                      border-b border-gray-200
+                      bg-gray-50
+                      text-center text-xs
+                      leading-4
+                      font-medium
+                      text-gray-500
+                      uppercase
+                      tracking-wider
+                    "
                   >
                     Namn
                   </th>
                   <th
-                    class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-center text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                    class="
+                      px-6
+                      py-3
+                      border-b border-gray-200
+                      bg-gray-50
+                      text-center text-xs
+                      leading-4
+                      font-medium
+                      text-gray-500
+                      uppercase
+                      tracking-wider
+                    "
                   >
                     Vikt (kg)
                   </th>
                   <th
-                    class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-center text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                    class="
+                      px-6
+                      py-3
+                      border-b border-gray-200
+                      bg-gray-50
+                      text-center text-xs
+                      leading-4
+                      font-medium
+                      text-gray-500
+                      uppercase
+                      tracking-wider
+                    "
                   >
                     Datum
                   </th>
