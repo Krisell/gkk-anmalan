4c0bd848dd0de11c310b6645919c646bc316888c
Martin Krisell <martin.krisell@gmail.com>
2021-10-17 14:01:38 +0200

    Show unanswered events on top

--- resources/js/components/Events.vue
+++ resources/js/components/Events.vue
@@ -3,7 +3,7 @@
     <h1 class="text-center text-3xl font-thin mb-6">Funktionärsanmälan</h1>
 
     <div v-if="events.length">
-      <div v-for="event in events" :key="event.id" class="flex align-center mb-6">
+      <div v-for="event in orderedEvents" :key="event.id" class="flex align-center mb-6">
         <gkk-event-card
           @click="location(`/events/${event.id}`)"
           :event="event"
@@ -23,6 +23,21 @@
 <script>
 export default {
   props: ['events', 'user-registrations'],
+  computed: {
+    orderedEvents() {
+      return this.events.slice().sort((a, b) => {
+        if (this.userRegistrations.find((reg) => Number(reg.event_id) === Number(b.id))) {
+          return -1
+        }
+
+        if (this.userRegistrations.find((reg) => Number(reg.event_id) === Number(a.id))) {
+          return 1
+        }
+
+        return 0
+      })
+    },
+  },
   methods: {
     registrationFor(event) {
       return this.userRegistrations.find((registration) => Number(registration.event_id) === Number(event.id))
