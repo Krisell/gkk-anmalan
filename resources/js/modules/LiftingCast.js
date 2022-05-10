46ae89947b5f855648df09cb144a40f6607e9f89
Martin Krisell <martin.krisell@gmail.com>
2022-05-10 14:56:44 +0200

    Update LiftingCast export

--- resources/js/modules/LiftingCast.js
+++ resources/js/modules/LiftingCast.js
@@ -1,4 +1,5 @@
 import File from './File.js'
+import Parser from '@krisell/parser'
 
 const LiftingCast = {
   download(lifters) {
@@ -14,9 +15,14 @@ const LiftingCast = {
       const flight = lifter.gender === 'Män' ? 'B' : 'A'
       const gender = lifter.gender === 'Män' ? 'Male' : 'Female'
 
-      let birthdate = LiftingCast.getBirthdate(lifter.licence_number)
+      const birthdate = LiftingCast.getBirthdate(lifter.licence_number)
 
-      csv += `"${lifter.user.first_name} ${lifter.user.last_name}","GKK",,"1","1","${flight}","${birthdate}","","${gender}","Raw","${lifter.gender}","Open",,"","",,,,"N","","","",,"","","","",""\n`
+      const events = Parser.json(lifter.events)
+
+      let division = events.kbp ? 'BP' : 'SL'
+      division += lifter.gender === 'Män' ? ' Herr' : ' Dam'
+
+      csv += `"${lifter.user.first_name} ${lifter.user.last_name}","GKK",,"1","1","${flight}","${birthdate}","","${gender}","Raw","${division}","Open",,"","",,,,"N","","","",,"","","","",""\n`
     })
 
     File.save('liftingcast-import.csv', csv, 'data:text/csv;charset=utf-8')
