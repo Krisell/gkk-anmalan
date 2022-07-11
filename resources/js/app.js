4fddb344e713c4f79f75c7938e3a5a05f1c632f2
Martin Krisell <martin.krisell@gmail.com>
2022-07-11 18:47:04 +0200

    Update npm dependencies

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,23 @@ import VModal from 'vue-js-modal'
 import VTooltip from 'v-tooltip'
 import ToggleButton from 'vue-js-toggle-button'
 
-require('./bootstrap')
+window.axios = require('axios')
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+
+window.firebase.initializeApp({
+  apiKey: 'AIzaSyCe-FSgSLultZJpVCVJwXOKDGrY31IFZ4E', // firebaseConfig apiKey is not a secret: https://stackoverflow.com/questions/37482366/is-it-safe-to-expose-firebase-apikey-to-the-public, https://firebase.google.com/docs/web/setup
+  authDomain: 'goteborg-kraftsportklubb.firebaseapp.com',
+  databaseURL: 'https://goteborg-kraftsportklubb.firebaseio.com',
+  projectId: 'goteborg-kraftsportklubb',
+  storageBucket: 'goteborg-kraftsportklubb.appspot.com',
+  messagingSenderId: '52915573324',
+  appId: '1:52915573324:web:2d7620cec6430501c35ae9',
+  measurementId: 'G-B8J19RFZ1R',
+})
+
+if (typeof window.firebase.analytics === 'function') {
+  window.firebase.analytics()
+}
 
 window.Vue = Vue
 Vue.config.ignoredElements = ['trix-editor']
