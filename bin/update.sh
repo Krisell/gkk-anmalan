f59c9b762c84e31cc72d4422660e1e5a1521c6ea
Martin Krisell <martin.krisell@gmail.com>
2022-08-13 12:52:49 +0200

    Update deploy script

--- bin/update.sh
+++ bin/update.sh
@@ -4,7 +4,7 @@ git pull
 
 cp /customers/d/f/2/gkk-styrkelyft.se/httpd.private/anmalan/public/css/* /customers/d/f/2/gkk-styrkelyft.se/httpd.www/anmalan/css
 cp /customers/d/f/2/gkk-styrkelyft.se/httpd.private/anmalan/public/js/* /customers/d/f/2/gkk-styrkelyft.se/httpd.www/anmalan/js
-cp -r /customers/d/f/2/gkk-styrkelyft.se/httpd.private/anmalan/public/build /customers/d/f/2/gkk-styrkelyft.se/httpd.www/anmalan/build
+cp -r /customers/d/f/2/gkk-styrkelyft.se/httpd.private/anmalan/public/build /customers/d/f/2/gkk-styrkelyft.se/httpd.www/anmalan
 php /customers/d/f/2/gkk-styrkelyft.se/httpd.private/anmalan/artisan config:cache
 php /customers/d/f/2/gkk-styrkelyft.se/httpd.private/anmalan/artisan route:cache
 
