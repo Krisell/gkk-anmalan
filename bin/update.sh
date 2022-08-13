6d50532c3f7bb4c6d337dd4ec5b2366b807c3a8f
Martin Krisell <martin.krisell@gmail.com>
2022-08-13 12:47:30 +0200

    Remove unnecessary steps from deploy script

--- bin/update.sh
+++ bin/update.sh
@@ -5,8 +5,6 @@ git pull
 cp /customers/d/f/2/gkk-styrkelyft.se/httpd.private/anmalan/public/css/* /customers/d/f/2/gkk-styrkelyft.se/httpd.www/anmalan/css
 cp /customers/d/f/2/gkk-styrkelyft.se/httpd.private/anmalan/public/js/* /customers/d/f/2/gkk-styrkelyft.se/httpd.www/anmalan/js
 cp -r /customers/d/f/2/gkk-styrkelyft.se/httpd.private/anmalan/public/build /customers/d/f/2/gkk-styrkelyft.se/httpd.www/anmalan/build
-cp /customers/d/f/2/gkk-styrkelyft.se/httpd.private/anmalan/public/mix-manifest.json /customers/d/f/2/gkk-styrkelyft.se/httpd.www/anmalan/mix-manifest.json
-cp /customers/d/f/2/gkk-styrkelyft.se/httpd.private/anmalan/public/manifest.json /customers/d/f/2/gkk-styrkelyft.se/httpd.www/anmalan/manifest.json
 php /customers/d/f/2/gkk-styrkelyft.se/httpd.private/anmalan/artisan config:cache
 php /customers/d/f/2/gkk-styrkelyft.se/httpd.private/anmalan/artisan route:cache
 
