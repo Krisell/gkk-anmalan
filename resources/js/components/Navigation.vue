ba8fc8d0b9a1fb743d539929fc10706e130879bb
Martin Krisell <martin.krisell@gmail.com>
2022-09-19 18:32:58 +0200

    Skip unanswered badge for competitions

--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -15,7 +15,6 @@
         @click="location('/competitions')"
         description="Tävlingsanmälan"
         icon="list-ul"
-        :unanswered="unanswered.competitions"
       ></gkk-action-card>
       <gkk-action-card
         v-if="user"
