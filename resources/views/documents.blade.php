e4978af86b29708f2814a08aef21d509334ae55d
Martin Krisell <martin.krisell@gmail.com>
2022-10-08 15:25:13 +0200

    Update website information

--- resources/views/documents.blade.php
+++ resources/views/documents.blade.php
@@ -11,10 +11,79 @@ background-position-y: center;" class="flex items-center">
         <div class="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8">
           <div class="space-y-12">
             <div class="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
-                <div class="flex items-center">
-                <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl">Dokument & Träningsprogram</h2>
+                <div>
+                  <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl">Dokument</h2>
+                  <div class="mt-4">
+                    <i class="fa fa-file-pdf-o"></i>
+                    <a class="underline" target="_blank" href="https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/documents%2FAntidopingavtal.pdf?alt=media&token=f43b48b7-62e8-405e-8b3e-f75ac2c465c6">Antidopingavtal mellan medlem och förening (PDF)</a>
+                  </div>
+                  <div class="mt-4">
+                    <i class="fa fa-file-pdf-o"></i>
+                    <a class="underline" target="_blank" href="https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/documents%2FMedlemsavtal%20GKK%202022-09-20.pages.pdf?alt=media&token=52b3f7dd-e27c-49f2-a63d-0bdf2a0a0188">Medlemsavtal mellan medlem och förening (PDF)</a>
+                  </div>
+                  <div class="mt-4">
+                    <i class="fa fa-file-pdf-o"></i>
+                    <a class="underline" target="_blank" href="https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/uploaded%2FN0TGPEf5Nto75XNm2Nux7Lpojez7pz.pdf?alt=media&token=3204252d-8c0c-43f0-baa2-de7b11ca277c">Föreningens stadgar, antagna 2022-02-19 (PDF)</a>
+                  </div>
+                  <div class="mt-4">
+                    <i class="fa fa-file-pdf-o"></i>
+                    <a class="underline" target="_blank" href="https://www.gkk-styrkelyft.se/wp-content/uploads/2020/03/gkk-integritetspolicy.pdf">Föreningens integritetspolicy (PDF)</a>
+                  </div>
+                  <div class="mt-4">
+                    <i class="fa fa-link"></i>
+                    <a class="underline" target="_blank" href="https://www.styrkelyft.se/dokument/dokument-blanketter">Dokument för tävling mm på förbundets hemsida</a>
+                  </div>
                 </div>
-                <p class="text-xl text-gray-500">TODO: Motsvarande det som idag ligger på "Dokument" på hemsidan.</p>
+                
+                <div>
+                  <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl mt-12">Träningsprogram</h2>
+                  <div class="flex mt-4">
+                    <a target="_blank" href="https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/documents%2FBeard-Press%202021-02-14.xlsx?alt=media&token=30924370-12aa-4ddf-8bf1-2fd45cc70717">
+                      <img width="200px" src="https://www.gkk-styrkelyft.se/wp-content/uploads/2021/02/beardpress-logo-excel.png">
+                    </a>
+                    <div class="p-4">
+                      <b>Beard Press</b><br>
+                      Ett bänkpressprogram av Karl Malmberg (@kraftkarlos)<br>
+                      <span class="text-gray-500 mt-2">Excelfil</span>
+                    </div>
+                  </div>
+                  <div class="flex mt-4">
+                    <a target="_blank" href="https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/documents%2FBa%CC%88nkprogram%20CalleO%CC%88berg.xlsx?alt=media&token=731f217f-fe83-4950-88b6-402515862445">
+                      <img width="220px" src="https://www.gkk-styrkelyft.se/wp-content/uploads/2021/07/hemside-bild.png">
+                    </a>
+                    <div class="p-4">
+                      <b>Carl Öbergs bänkpressprogram</b><br>
+                      <span class="text-gray-500 mt-2">Excelfil</span>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl mt-12">Grafik</h2>
+                  <div class="flex items-center mt-2">
+                    <img width="100px" src="http://www.gkk-styrkelyft.se/wp-content/uploads/2014/08/Tv%C3%A5f%C3%A4rg-p%C3%A5-ljus-bakgrund.png">
+                    <div class="ml-2">Tvåfärg mot ljus bakgrund</div>
+                  </div>
+                  <div class="flex items-center mt-2">
+                    <img width="100px" src="https://www.gkk-styrkelyft.se/wp-content/uploads/2014/08/Tv%C3%A5f%C3%A4rg-p%C3%A5-m%C3%B6rk-bakgrund-300x300.png">
+                    <div class="ml-2">Tvåfärg mot mörk bakgrund (för tryck och utskrift)</div>
+                  </div>
+                  <div class="flex items-center mt-2">
+                    <img width="100px" src="http://www.gkk-styrkelyft.se/wp-content/uploads/2014/08/Tv%C3%A5f%C3%A4rg-p%C3%A5-m%C3%B6rk-bakgrund-transparent.png">
+                    <div class="ml-2">Tvåfärg mot mörk bakgrund (transparent)</div>
+                  </div>
+                  <div class="flex items-center mt-2">
+                    <img width="100px" src="http://www.gkk-styrkelyft.se/wp-content/uploads/2014/08/Monokrom-p%C3%A5-m%C3%B6rk-bakgrund.png">
+                    <div class="ml-2">Monokrom mot mörk bakgrund</div>
+                  </div>
+                  <div class="mt-4"> 
+                    <i class="fa fa-file-pdf-o"></i>
+                    <a class="underline" target="_blank" href="http://www.gkk-styrkelyft.se/wp-content/uploads/2014/08/Monokrom-p%C3%A5-m%C3%B6rk-bakgrund.pdf">Monokrom mot mörk bakgrund för tryck/utskrift (PDF)</a>
+                  </div>
+                  <div class="mt-4">
+                    <i class="fa fa-file-pdf-o"></i>
+                    <a class="underline" target="_blank" href="http://www.gkk-styrkelyft.se/wp-content/uploads/2014/08/Tv%C3%A5f%C3%A4rg-p%C3%A5-m%C3%B6rk-bakgrund.pdf">Tvåfärg på mörk bakgrund för tryck/utskrift (PDF)</a>
+                  </div>
+                </div>
             </div>
             <ul role="list" class="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8">
               <li>
--- resources/views/powerlifting.blade.php
+++ resources/views/powerlifting.blade.php
@@ -11,13 +11,38 @@ background-position-y: center;" class="flex items-center">
         <div class="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8">
           <div class="space-y-12">
             <div class="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
-                <div class="flex items-center">
-                <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl">Om Styrkelyft</h2>
-                </div>
-                <p class="text-xl leading-normal text-gray-500">I styrkelyft tävlar man i grenarna knäböj, bänkpress och marklyft. Man kan också tävla i enbart bänkpress. Det finns tävlingar där särskild utrustning, exempelvis knälindor och böjdräkt, är tillåtet, och det finns tävlingar i så kallad "klassisk styrkelyft" där ingen hjälpande utrustning utöver knävärmare och handledslindor är tillåtet. Den klassiska disciplinen har idag flest utövare och är en lämplig utgångpunkt för en nybörjare.</p>
-                <p class="text-xl leading-normal text-gray-400">TODO: Här kan det vara lämpligt att visa bilder/filmer på de tre lyften.</p>
-                <p class="text-xl leading-normal text-gray-400">TODO: Vill man kanske nämna något om idrottens historia?</p>
-                <p class="text-xl leading-normal text-gray-400">TODO: Den nuvarande hemsidans information om hur en tävling går till kanske kan placeras här, alternativt som ett separat dokument?</p>
+              <div class="flex items-center">
+              <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl">Om Styrkelyft</h2>
+              </div>
+              <p class="text-xl leading-normal text-gray-500">I styrkelyft tävlar man i grenarna knäböj, bänkpress och marklyft. Man kan också tävla i enbart bänkpress. Det finns tävlingar där särskild utrustning, exempelvis knälindor och dräkt, är tillåtet, och det finns tävlingar i så kallad "klassisk styrkelyft" där ingen hjälpande utrustning utöver knävärmare och handledslindor är tillåtet. Den klassiska disciplinen har idag flest utövare och är också en lämplig utgångpunkt för en nybörjare.</p>
+              <p class="text-xl leading-normal text-gray-400">Här är tanken att visa bilder på de tre lyften.</p>
+            </div>
+            <div class="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
+              <div class="flex items-center">
+              <h3 class="text-2xl font-extrabold tracking-tight">Att tävla i Styrkelyft</h3>
+              </div>
+              <p class="text-xl leading-normal text-gray-500">
+                Det finns fyra olika tävlingsdiscipliner inom idrotten som lyftaren kan välja mellan:
+              </p>
+                <ul class="px-8">
+                  <li class="list-disc text-xl leading-normal text-gray-500">Klassisk Styrkelyft (total i Knäböj + Bänkpress + Marklyft)</li>
+                  <li class="list-disc text-xl leading-normal text-gray-500">Klassisk Bänkpress</li>
+                  <li class="list-disc text-xl leading-normal text-gray-500">Utrustad Styrkelyft (total i Utrustad Knäböj + Utrustad Bänkpress + Utrustad Marklyft)</li>
+                  <li class="list-disc text-xl leading-normal text-gray-500">Utrustad Bänkpress</li>
+                </ul>
+
+              <p class="text-xl leading-normal text-gray-500">
+                Samtliga dessa har mästerskap på distriktsnivå, nationell nivå och internationellt. Det finns även i regel mästerskap för ungdomar, juniorer och veteraner i respektive disciplin.
+              </p>
+              <p class="text-xl leading-normal text-gray-500">
+                Man kan tävla antingen i en så kallad <b>serietävling</b> eller i ett <b>mästerskap</b>. I mästerskap tävlar man om placeringar och medaljer. I serietävlingar samlar man poäng till föreningens lag, eller tävlar för att se hur mycket man klarar. Till vissa mästerskap (ex. SM) måste man kvala in, och det kan man göra genom att klara uppsatta kvalgränser vid en serietävling.
+              </p>
+              <p class="text-xl leading-normal text-gray-500">
+                Göteborg Kraftsportklubb arrangerar ofta serietävlingar på hemmaplan på Friskis och Svettis i Majorna, då hela föreningen är engagerad och medlemmarna antingen är med och tävlar eller ställer upp som funktionär. Om föreningens lag presterar bra under året kan de gå till final i lag-SM där SM-medaljer står på spel.
+              </p>
+              <p class="text-xl leading-normal text-gray-500">
+                Vid mästerskap anmäler man sig till en viktklass och man måste väga in 2 timmar före tävlingsstart för att få delta. Vid serietävlingar finns inga viktklasser utan där beräknas lagpoängen baserat på vikt på stången och dagens kroppsvikt.
+              </p>
             </div>
             <ul role="list" class="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8">
               <li>
