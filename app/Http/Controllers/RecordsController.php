04de583246ea654d7192a87e40918caae2b80f5d
Martin Krisell <martin.krisell@gmail.com>
2022-10-20 20:06:57 +0200

    Remove redirect to old page for records

--- app/Http/Controllers/RecordsController.php
+++ app/Http/Controllers/RecordsController.php
@@ -8,10 +8,6 @@ class RecordsController extends Controller
 {
     public function index()
     {
-        if (config('gkk.no-database-yet')) {
-            return redirect(config('gkk.redirect').'/records');
-        }
-
         return view('records', [
             'results' => Result::with('user:id,first_name,last_name')->get(),
             'site' => 'records',
