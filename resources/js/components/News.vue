f4431e9ffecfaef6d6ddb7feb6bc6377b80b18ad
Martin Krisell <martin.krisell@gmail.com>
2022-10-22 17:28:03 +0200

    Fix button to create email from news item

--- resources/js/components/News.vue
+++ resources/js/components/News.vue
@@ -2,7 +2,7 @@
   <div class="max-w-3xl mx-auto p-4">
     <h1 class="text-3xl font-thin mb-2" v-if="news.length">Nyheter</h1>
     <div class="mb-2" @click="location('/admin/news')" v-if="isAdmin">
-      <Button type="danger" style="padding: 5px; font-size: 10px">Skapa nyhet</Button>
+      <Button type="danger" class="text-xs">Skapa nyhet</Button>
     </div>
 
     <NewsItem
--- resources/js/components/NewsItem.vue
+++ resources/js/components/NewsItem.vue
@@ -1,20 +1,26 @@
 <template>
   <div>
-    <div
-      v-tooltip.left="'Gör epostutskick'"
-      class="absolute top-0 right-4 mt-2 cursor-pointer"
-      @click="location(`/admin/news/email/${item.id}`)"
-      v-if="isAdmin"
-    >
-      <i class="fa fa-envelope"></i>
-    </div>
     <h1 @click="expanded = !expanded" style="margin: 20px 0; cursor: pointer">
       {{ item.title }} <span style="font-size: 12px">{{ publishDate }}</span>
       <span v-if="!expanded" class="text-xs ml-4 text-gray-500">Klicka för att visa</span>
     </h1>
     <div v-if="expanded" v-html="item.body"></div>
-    <div class="flex justify-end" @click="location(`/admin/news/${item.id}`)" v-if="isAdmin">
-      <Button type="danger" style="padding: 5px; font-size: 10px">Redigera nyhet</Button>
+    <div class="flex justify-end items-center" v-if="isAdmin">
+      <Button 
+        type="danger" 
+        class="text-xs"
+        @click="location(`/admin/news/${item.id}`)"
+      >
+        Redigera nyhet
+      </Button>
+      <div
+        v-tooltip.top="'Gör epostutskick'"
+        class="ml-4 cursor-pointer"
+        @click="location(`/admin/news/email/${item.id}`)"
+        v-if="isAdmin"
+      >
+        <i class="fa fa-envelope text-xl"></i>
+      </div>
     </div>
   </div>
 </template>
