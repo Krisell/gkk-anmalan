0b379b76f4bcfb8b82437d8e2bf356358200c503
Martin Krisell <martin.krisell@gmail.com>
2022-10-29 12:21:09 +0200

    Add additional test

---
+++ tests/Feature/CompetitionTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Competition;
+
+it('can show all competitions for signed in users', function () {
+    $this->get('/competitions')->assertRedirect('/login');
+
+    login();
+
+    $competition = Competition::factory()->create([
+        'date' => now()->addDays(7),
+        'publish_count' => true,
+    ]);
+
+    $this->get('/competitions')->assertSee($competition->name);
+});
