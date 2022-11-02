fbdab193eeb63f05feb4b2c4795b94206fd5fcc3
Martin Krisell <martin.krisell@gmail.com>
2022-11-02 18:49:19 +0100

    Update DM start lists

--- resources/views/dm2022.blade.php
+++ resources/views/dm2022.blade.php
@@ -21,8 +21,8 @@ background-position-y: center; max-height: 50vh;" class="flex items-center">
                 <a 
                   target="_blank"
                   class="inline-flex items-center px-4 py-2 border border-gkk hover:bg-gkk hover:text-white leading-5 font-medium rounded-md focus:outline-none focus:shadow-outline-indigo transition duration-150 ease-in-out" 
-                  href="https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/static%2FStartlistor%20rev.%202022-10-29.pdf?alt=media&token=31549bb3-7f27-41de-810b-ebeffdfb3f25">
-                  <i class="fa fa-file-pdf-o mr-2"></i>Startlistor och tävlingstider (rev. 2022-10-29)
+                  href="https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/static%2FStartlistor%20rev.%202022-11-02.pdf?alt=media&token=968ba854-656c-45fc-874b-7251d2843ee5">
+                  <i class="fa fa-file-pdf-o mr-2"></i>Startlistor och tävlingstider (rev. 2022-11-02)
                 </a>
                 <p class="mt-4 text-xl leading-normal text-gray-500"><b>Observera ändrade starttider från inbjudan på grund av stort deltagarantal.</b></p>
                 <p class="mt-4 text-xl leading-normal text-gray-500"><b>Plats:</b> Göteborg Kraftsportklubb, Karl Johansgatan 152, 414 51 Göteborg</p>
@@ -34,8 +34,8 @@ background-position-y: center; max-height: 50vh;" class="flex items-center">
                 <a 
                   target="_blank"
                   class="inline-flex items-center px-4 py-2 border border-gkk hover:bg-gkk hover:text-white leading-5 font-medium rounded-md focus:outline-none focus:shadow-outline-indigo transition duration-150 ease-in-out" 
-                  href="https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/static%2FStartlistor%20rev.%202022-10-29.pdf?alt=media&token=31549bb3-7f27-41de-810b-ebeffdfb3f25">
-                  <i class="fa fa-file-pdf-o mr-2"></i>Startlistor och tävlingstider (rev. 2022-10-29)
+                  href="https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/static%2FStartlistor%20rev.%202022-11-02.pdf?alt=media&token=968ba854-656c-45fc-874b-7251d2843ee5">
+                  <i class="fa fa-file-pdf-o mr-2"></i>Startlistor och tävlingstider (rev. 2022-11-02)
                 </a>
                 <p class="text-xl leading-normal text-gray-500">
                   Resultatlistor publiceras här efter avslutad tävling.
