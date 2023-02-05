788b21e39c8af740df18e312498d26911c3b1c3a
Martin Krisell <martin.krisell@gmail.com>
2023-02-05 09:31:36 +0100

    Add documents for Årsmöte

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -15,12 +15,30 @@ background-position-y: center; max-height: 50vh;" class="flex items-center">
                   <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl">Välkommen till Göteborg Kraftsportklubb</h2>
                 </div>
 
-                <a 
-                  target="_blank"
-                  class="inline-flex items-center px-4 py-2 border border-gkk bg-gkk text-white leading-5 font-medium rounded-md focus:outline-none focus:shadow-outline-indigo transition duration-150 ease-in-out" 
-                  href="https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/uploaded%2F4UcDqNytWB8qo8RqLj7wK93SieyY9X.pdf?alt=media&token=363cd549-67a6-4880-a92b-ae65c1c5acaf">
-                  <i class="fa fa-file-pdf-o mr-2"></i>Kallelse och föredragningslista årsmöte 2023-02-11 kl 10.00
-                </a>
+                <div>
+                  <a 
+                    target="_blank"
+                    class="inline-flex items-center px-4 py-2 border border-gkk bg-gkk text-white leading-5 font-medium rounded-md focus:outline-none focus:shadow-outline-indigo transition duration-150 ease-in-out" 
+                    href="https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/uploaded%2F4UcDqNytWB8qo8RqLj7wK93SieyY9X.pdf?alt=media&token=363cd549-67a6-4880-a92b-ae65c1c5acaf">
+                    <i class="fa fa-file-pdf-o mr-2"></i>Kallelse och föredragningslista årsmöte 2023-02-11 kl 10.00
+                  </a>
+                </div>
+                <div>
+                  <a 
+                    target="_blank"
+                    class="inline-flex items-center px-4 py-2 border border-gkk bg-white text-gkk leading-5 font-medium rounded-md focus:outline-none focus:shadow-outline-indigo transition duration-150 ease-in-out" 
+                    href="https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/uploaded%2FhPHW4i2RwwFcCsjV3lyC8ov7fneeLE.pdf?alt=media&token=96ee94fc-2f51-433c-8aaa-7dfae6154598">
+                    <i class="fa fa-file-pdf-o mr-2"></i>Verksamhetsberättelse 2022. Läs gärna och kom med feedback på ev. fel, tillägg och förbättringar.
+                  </a>
+                </div>
+                <div>
+                  <a 
+                    target="_blank"
+                    class="inline-flex items-center px-4 py-2 border border-gkk bg-white text-gkk leading-5 font-medium rounded-md focus:outline-none focus:shadow-outline-indigo transition duration-150 ease-in-out" 
+                    href="https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/uploaded%2FZ1g0KkbTb7ePYq7h0QSr02zReZGeKN.pdf?alt=media&token=7fea9ac6-652d-4c62-9076-39a423d1bc0d">
+                    <i class="fa fa-file-pdf-o mr-2"></i>Verksamhetsplan 2023
+                  </a>
+                </div>
 
                 <p class="text-xl leading-normal text-gray-500">
                   Göteborg Kraftsportklubb (GKK) är en idrottsförening som bildades 1933. Vi ägnar oss åt idrotten Styrkelyft och tränar och tävlar därmed i knäböj, bänkpress och marklyft. Sedan 2022 har vi också ett antal medlemmar som tränar tyngdlyftning med ambitionen att tävla. I föreningen finns allt från motionärer till elitaktiva på högsta nivå. Vi tränar tillsammans, hjälper varandra och tror på att styrka kommer från gemenskap.
