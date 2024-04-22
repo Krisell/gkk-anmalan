6ab34ca0ad8a0e50fd2649ac9894d57fd3b7dded
Martin Krisell <martin.krisell@gmail.com>
2024-04-22 18:14:48 +0200

    Update GM start lists

--- resources/views/gm2024.blade.php
+++ resources/views/gm2024.blade.php
@@ -28,10 +28,10 @@ background-position-y: center; max-height: 50vh;" class="flex items-center">
                   <i class="fa fa-file-pdf-o mr-2"></i>Inbjudan Götalandsmästerskapen 2024
                 </a>
                 <div></div>
-                <a target="_blank" href="https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/static%2FStartlistor_GM_2024_2024-04-17.pdf?alt=media&token=da105918-d4d2-4b2d-ad54-2622681f6be7">
+                <a target="_blank" href="https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/static%2FAnma%CC%88lda%20GM%202024%202024-04-22.pdf?alt=media&token=6d8bcf6c-dcec-417b-92af-627042c2eb44">
                   <p 
                   class="inline-flex items-center px-4 py-2 border border-gkk bg-white text-gkk leading-5 font-medium rounded-md focus:outline-none focus:shadow-outline-indigo transition duration-150 ease-in-out hover:bg-gkk hover:text-white" 
-                    >Startlistor och tävlinstider (uppd. 2024-04-17)</p>
+                    >Startlistor och tävlinstider (uppd. 2024-04-22)</p>
                 </a>
                 {{-- <p class="mt-4 text-xl leading-normal text-gray-500"><b>Tävlingen sker med Eleiko-ställning av nyare modell.</b></p> --}}
                 <p class="mt-4 text-xl leading-normal text-gray-500"><b>Plats:</b> Göteborg Kraftsportklubb, Karl Johansgatan 152, 414 51 Göteborg</p>
