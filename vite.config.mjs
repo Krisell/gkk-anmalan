9824547e173f67698bdfc97f3a239279f4ba3879
Martin Sandström Krisell <martin.krisell@gmail.com>
2024-05-26 17:53:12 +0200

    Simplify Vite configuration

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue2'
-import { resolve } from 'path'
-import { homedir } from 'os'
-import fs from 'fs'
 
 export default defineConfig({
   plugins: [
-    laravel({
-      input: ['resources/css/main.css', 'resources/js/app.js'],
-      refresh: true,
+    process.env.VITEST
+      ? null
+      : laravel({
+        valetTls: 'gkk.test',
+        input: ['resources/css/main.css', 'resources/js/app.js'],
+        refresh: true,
     }),
     vue({
       template: {
@@ -20,34 +20,9 @@ export default defineConfig({
       },
     }),
   ],
-  server: detectServerConfig('gkk.test'),
   test: {
     globals: true,
     environment: 'jsdom',
     include: ['resources/js/test/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
 })
-
-// We'll have to figure out a better way to run the client dev server, since not everyone uses Valet
-// Maybe the dev server can have it's own ssl certs?
-function detectServerConfig(host) {
-  const keyPath = resolve(homedir(), `.config/valet/Certificates/${host}.key`)
-  const certificatePath = resolve(homedir(), `.config/valet/Certificates/${host}.crt`)
-
-  if (!fs.existsSync(keyPath)) {
-    return {}
-  }
-
-  if (!fs.existsSync(certificatePath)) {
-    return {}
-  }
-
-  return {
-    hmr: { host },
-    host,
-    https: {
-      key: fs.readFileSync(keyPath),
-      cert: fs.readFileSync(certificatePath),
-    },
-  }
-}
