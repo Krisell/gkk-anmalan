774417326ab30af0f3743396dd3486d5711c7964
Martin Sandström Krisell <martin.krisell@gmail.com>
2024-06-22 16:55:12 +0200

    Update to Laravel 11

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,13 +11,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
-    {
-        if ($this->app->environment('local')) {
-            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
-            $this->app->register(TelescopeServiceProvider::class);
-        }
-    }
+    public function register() {}
 
     /**
      * Bootstrap any application services.
