33750520176884d012d594db3f8840454e91089b
Martin Sandström Krisell <martin.krisell@gmail.com>
2024-10-06 17:23:26 +0200

    Remove unused mail config

--- config/mail.php
+++ config/mail.php
@@ -8,18 +8,4 @@ return [
         ],
     ],
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-
-    'port' => env('MAIL_PORT', 587),
-
-    'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-
-    'username' => env('MAIL_USERNAME'),
-
-    'password' => env('MAIL_PASSWORD'),
-
-    'sendmail' => '/usr/sbin/sendmail -bs',
-
-    'log_channel' => env('MAIL_LOG_CHANNEL'),
-
 ];
