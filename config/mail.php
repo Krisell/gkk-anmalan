9ac09dc70bfb2f028ea4513734d2421ef044a35e
Martin Sandström Krisell <martin.krisell@gmail.com>
2024-10-06 17:25:10 +0200

    Add email sender details

--- config/mail.php
+++ config/mail.php
@@ -8,4 +8,9 @@ return [
         ],
     ],
 
+    'from' => [
+        'address' => env('MAIL_FROM_ADDRESS', 'info@gkk-styrkelyft.se'),
+        'name' => env('MAIL_FROM_NAME', 'Göteborg Kraftsportklubb'),
+    ],
+
 ];
