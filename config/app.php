27128c5e73748bc15ea65344dc0e0586a8269956
Shift <shift@laravelshift.com>
2024-10-06 07:27:51 +0000

    Re-register service providers

--- config/app.php
+++ config/app.php
@@ -7,24 +7,6 @@ return [
 
     'mix_url' => env('MIX_URL'),
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Laravel Framework Service Providers...
-         */
-
-        /*
-         * Package Service Providers...
-         */
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-    ])->toArray(),
 
     'aliases' => Facade::defaultAliases()->merge([
         'Redis' => Illuminate\Support\Facades\Redis::class,
