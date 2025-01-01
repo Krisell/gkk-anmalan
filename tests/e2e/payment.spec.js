3c3337609e79fe178bfc77007fc245605899c78e
Martin Sandström Krisell <martin.krisell@gmail.com>
2025-01-01 13:14:21 +0100

    Fix year in tests

--- tests/e2e/payment.spec.js
+++ tests/e2e/payment.spec.js
@@ -7,30 +7,30 @@ test('Payments only show status before being synced to Fortnox', async ({ page }
     await create(page, 'Payment', { 
         user_id: user.id, 
         type: 'MEMBERSHIP', 
-        year: new Date().getFullYear() + 1,
+        year: 2025,
         sek_amount: 123,
     })
 
     const payment = await create(page, 'Payment', { 
         user_id: user.id, 
         type: 'SSFLICENSE', 
-        year: new Date().getFullYear() + 2,
+        year: 2025,
         sek_amount: 456,
     })
 
     await page.goto('/profile')
 
     await expect(await page.getByText('Medlemsavgift 2025123 SEK INVÄNTAR FAKTURERING')).toBeVisible()
-    await expect(await page.getByText('Tävlingslicens 2026456 SEK INVÄNTAR FAKTURERING')).toBeVisible()
+    await expect(await page.getByText('Tävlingslicens 2025456 SEK INVÄNTAR FAKTURERING')).toBeVisible()
 })
 
-test.only('Payments show status and invoice link after being synced to Fortnox', async ({ page }) => {
+test('Payments show status and invoice link after being synced to Fortnox', async ({ page }) => {
     const user = await login(page)
 
     await create(page, 'Payment', { 
         user_id: user.id, 
         type: 'MEMBERSHIP', 
-        year: new Date().getFullYear() + 1,
+        year: 2025,
         fortnox_invoice_document_number: 123,
         sek_amount: 123,
     })
@@ -38,7 +38,7 @@ test.only('Payments show status and invoice link after being synced to Fortnox',
     const payment = await create(page, 'Payment', { 
         user_id: user.id, 
         type: 'SSFLICENSE', 
-        year: new Date().getFullYear() + 2,
+        year: 2025,
         fortnox_invoice_document_number: 456,
         sek_amount: 456,
     })
@@ -46,7 +46,7 @@ test.only('Payments show status and invoice link after being synced to Fortnox',
     await page.goto('/profile')
 
     await expect(await page.getByText('Medlemsavgift 2025123 SEKINVÄNTAR BETALNINGKlicka för att öppna faktura')).toBeVisible()
-    await expect(await page.getByText('Tävlingslicens 2026456 SEKINVÄNTAR BETALNINGKlicka för att öppna faktura')).toBeVisible()
+    await expect(await page.getByText('Tävlingslicens 2025456 SEKINVÄNTAR BETALNINGKlicka för att öppna faktura')).toBeVisible()
     await expect(await page.getByText('Faktura har även skickats till din epost.').first()).toBeVisible()
     await expect(await page.getByText('Efter betalning kan det ta ett par dagar innan status uppdateras här.').first()).toBeVisible()
 })
