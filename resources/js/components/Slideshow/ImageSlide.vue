5c90542425198981ff5e606661ecfd27c0b525e8
Martin Sandström Krisell <martin.krisell@gmail.com>
2025-04-09 11:55:57 +0200

    Update ImageSlide style

--- resources/js/components/Slideshow/ImageSlide.vue
+++ resources/js/components/Slideshow/ImageSlide.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col items-center justify-center h-screen">
-        <img class="max-w-[80%] max-h-[80%]" :src="imageUrl">
+        <img class="max-w-[97%] max-h-[97%]" :src="imageUrl">
     </div>
 </template>
 
