ad14b50c52d54b4b8c6414f01c7e18f9df53c166
Martin Sandström Krisell <martin.krisell@gmail.com>
2025-04-09 12:11:10 +0200

    Add logging of slideshow device id

--- app/Http/Controllers/SlideshowController.php
+++ app/Http/Controllers/SlideshowController.php
@@ -6,7 +6,12 @@ class SlideshowController extends Controller
 {
     public function log()
     {
-        logger('SlideshowController log method called');
+        if (request('id') == null) {
+            logger('SlideshowController log method called from agent with missing id.');
+        } else {
+            logger('SlideshowController log method called from agent with id '.request('id'));
+        }
+
         logger('User Agent: '.request()->header('User-Agent'));
     }
 }
--- resources/js/components/Slideshow/Slideshow.vue
+++ resources/js/components/Slideshow/Slideshow.vue
@@ -5,10 +5,11 @@
         <ImageSlide v-if="slide == 2" imageUrl="https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/static%2Fsta%CC%88dschema.png?alt=media&token=8d8e4060-08a2-40df-804f-b4cc6b26467a" />
         <TextSlide v-if="slide == 3" text="goteborgkk.se" />
         <ImageSlide v-if="slide == 4" imageUrl="https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/static%2FLokalregler.png?alt=media&token=f6eacaf7-01cb-4983-81d8-5549679592e4" />
+        <ImageSlide v-if="slide == 5" imageUrl="https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/static%2FVa%CC%88lkommen.png?alt=media&token=448520ae-092b-43d6-b72e-4092cc55e435" />
 
         <!-- Add loading bar at the bottom, covering the full width -->
         <div class="fixed bottom-0 left-0 w-full h-2 bg-gray-300">
-            <div class="h-full bg-gkk transition-all duration-500" :style="{ width: (slide + 1) * 20 + '%' }"></div>
+            <div class="h-full bg-gkk transition-all duration-500" :style="{ width: (slide + 1) * (1/6) * 100 + '%' }"></div>
         </div>
     </div>
 </template>
@@ -20,13 +21,13 @@ import TextSlide from './TextSlide.vue'
 import axios from 'axios'
 
 setInterval(() => {
-    axios.post('/slideshow/log')
+    axios.post('/slideshow/log', { id: localStorage.getItem('id') })
 }, 60 * 1000)
 
 const slide = ref(0)
 
 const interval = setInterval(() => {
-    slide.value = (slide.value + 1) % 5
+    slide.value = (slide.value + 1) % 6
 }, 5000)
 
 // Reload page after 30 minutes. This might break the slideshow if the device is currently
