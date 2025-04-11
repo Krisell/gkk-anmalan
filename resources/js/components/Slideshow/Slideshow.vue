c34e70c362e2014d7c2523f7119e2296b23d9bd9
Martin Sandström Krisell <martin.krisell@gmail.com>
2025-04-11 13:59:17 +0200

    Add progress indicators to slideshow

--- resources/js/components/Slideshow/Slideshow.vue
+++ resources/js/components/Slideshow/Slideshow.vue
@@ -1,15 +1,16 @@
 <template>
     <div class="cursor-none">
-        <ImageSlide v-if="slide == 0" imageUrl="https://goteborg-kraftsportklubb.web.app/img/logo-min.png" />
-        <TextSlide v-if="slide == 1" text="GKKs slideshow - kommer snart!" />
-        <ImageSlide v-if="slide == 2" imageUrl="https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/static%2Fsta%CC%88dschema.png?alt=media&token=8d8e4060-08a2-40df-804f-b4cc6b26467a" />
-        <TextSlide v-if="slide == 3" text="goteborgkk.se" />
-        <ImageSlide v-if="slide == 4" imageUrl="https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/static%2FLokalregler.png?alt=media&token=f6eacaf7-01cb-4983-81d8-5549679592e4" />
-        <ImageSlide v-if="slide == 5" imageUrl="https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/static%2FVa%CC%88lkommen.png?alt=media&token=448520ae-092b-43d6-b72e-4092cc55e435" />
+        <div class="fixed bottom-0 right-0 p-4 text-gkk text-3xl opacity-70">
+            {{ currentSlide + 1 }} / {{ slides.length }}
+        </div>
 
-        <!-- Add loading bar at the bottom, covering the full width -->
-        <div class="fixed bottom-0 left-0 w-full h-2 bg-gray-300">
-            <div class="h-full bg-gkk transition-all duration-500" :style="{ width: (slide + 1) * (1/6) * 100 + '%' }"></div>
+        <template v-for="(slide, index) in slides">
+            <Component :is="slide.type" v-bind="slide.props" v-if="currentSlide == index" :key="index" class="w-full h-full" />
+        </template>
+
+        <div 
+            class="fixed bottom-0 left-0 h-3 bg-gkk opacity-60 transition-all"
+            :style="{ width: `${progress}%` }">
         </div>
     </div>
 </template>
@@ -20,6 +21,14 @@ import ImageSlide from './ImageSlide.vue'
 import TextSlide from './TextSlide.vue'
 import axios from 'axios'
 
+const slides = [
+    { id: 0, type: ImageSlide, props: { imageUrl: 'https://goteborg-kraftsportklubb.web.app/img/logo-min.png' } },
+    { id: 1, type: TextSlide, props: { text: 'GKKs slideshow - kommer snart!' } },
+    { id: 2, type: ImageSlide, props: { imageUrl: 'https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/static%2Fsta%CC%88dschema.png?alt=media&token=8d8e4060-08a2-40df-804f-b4cc6b26467a' } },
+    { id: 4, type: ImageSlide, props: { imageUrl: 'https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/static%2FLokalregler.png?alt=media&token=f6eacaf7-01cb-4983-81d8-5549679592e4' } },
+    { id: 5, type: ImageSlide, props: { imageUrl: 'https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/static%2FVa%CC%88lkommen.png?alt=media&token=448520ae-092b-43d6-b72e-4092cc55e435' } }
+]
+
 // If userAgent contains CrOS, set localStorage id to 'gkk-room'
 if (navigator.userAgent.includes('CrOS')) {
     localStorage.setItem('id', 'gkk-room')
@@ -29,11 +38,43 @@ setInterval(() => {
     axios.post('/slideshow/log', { id: localStorage.getItem('id') })
 }, 60 * 1000)
 
-const slide = ref(0)
+const currentSlide = ref(0)
+let startTime = Date.now()
+let slideInterval = null
+let progressInterval = null
+let progress = ref(0)
 
-const interval = setInterval(() => {
-    slide.value = (slide.value + 1) % 6
-}, 30 * 1000)
+function nextSlide() {
+    currentSlide.value = (currentSlide.value + 1) % slides.length
+    startTime = Date.now()
+    restartInterval()
+}
+
+function previousSlide() {
+    currentSlide.value = (currentSlide.value - 1 + slides.length) % slides.length
+    startTime = Date.now()
+    restartInterval()
+}
+
+restartInterval()
+function restartInterval() {
+    clearInterval(slideInterval)
+    clearInterval(progressInterval)
+    slideInterval = setInterval(nextSlide, 30 * 1000)
+
+    progress.value = 0
+    progressInterval = setInterval(() => {
+        progress.value += 1/6
+    }, 50)
+}
+
+document.body.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowRight') {
+        nextSlide()
+    } else if (e.key === 'ArrowLeft') {
+        previousSlide()
+    }
+})
 
 // Reload page after 10 minutes. This might break the slideshow if the device is currently
 // offline. That will be solved by adding a service worker for offline loading. It also looks
