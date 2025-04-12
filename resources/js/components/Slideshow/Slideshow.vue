9aeede34d7614b55cda1afebab68b8bdb62c2e03
Martin Sandström Krisell <martin.krisell@gmail.com>
2025-04-12 10:14:36 +0200

    Add Slide

--- resources/js/components/Slideshow/Slideshow.vue
+++ resources/js/components/Slideshow/Slideshow.vue
@@ -23,6 +23,7 @@ import axios from 'axios'
 const slides = [
     { id: 0, type: Slide, props: { image: 'https://goteborg-kraftsportklubb.web.app/img/logo-min.png' } },
     { id: 1, type: Slide, props: { text: 'GKKs slideshow - kommer snart!' } },
+    { id: 1, type: Slide, props: { text: 'Glöm inte anmäla er som funktionär till serie 2!' } },
     { id: 2, type: Slide, props: { text: 'Städschema', image: 'https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/static%2Fsta%CC%88dschema.png?alt=media&token=8d8e4060-08a2-40df-804f-b4cc6b26467a' } },
     { id: 4, type: Slide, props: { image: 'https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/static%2FLokalregler.png?alt=media&token=f6eacaf7-01cb-4983-81d8-5549679592e4' } },
     { id: 6, type: Slide, props: { text: 'Veckans meme', image: 'https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/static%2F27FE51DA-89FC-43DE-BB99-087315ADBC52.jpg?alt=media&token=e94405b0-5483-4705-b0d6-9c86f37d6a09' } },
