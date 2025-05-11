a5e3603dc3a80d1d33cad3e93e82d9f9d2392685
Martin Sandström Krisell <martin.krisell@gmail.com>
2025-05-11 09:31:35 +0200

    Adjust spacing for slideshow

--- resources/js/components/Slideshow/Slide.vue
+++ resources/js/components/Slideshow/Slide.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="flex flex-col items-center justify-center h-screen gap-4 max-h-screen">
-      <div v-if="text" class="text-6xl text-center text-gkk font-thin">
+      <div v-if="text" class="text-6xl text-center text-gkk font-thin mt-4">
             {{ text }}
       </div> 
       <div v-if="subText" class="text-2xl text-center text-gkk font-thin">
             {{ subText }}
       </div>
-      <img v-if="image" class="max-w-[97%] max-h-[97%] overflow-auto" :src="image">
+      <img v-if="image" class="max-w-[97%] max-h-[97%] overflow-auto mb-4" :src="image">
     </div>
 </template>
 
