d13bbaeed4dfbba9aca66272629718a6c6e7139f
Martin Sandström Krisell <25909128+Krisell@users.noreply.github.com>
2025-12-21 16:37:21 +0100

    Hide message while debugging

--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -5,9 +5,9 @@
         Du har obetalda avgifter. Klicka på "Profil" för mer information.<br>Efter betalning kan det ta några dagar innan denna notisering försvinner.
       </h3>
     </div>
-    <div v-if="user && helperCount === 0 && !user.explicit_registration_approval && isUserOlderThanOneMonth">
+    <div v-if="user && helperCount === 0 && isUserOlderThanOneMonth">
       <h3 class="text-center mt-6 text-md text-orange-600 max-w-[90%] mx-auto">
-        Du har inte hjälpt till som funktionär det senaste året. Detta kan påverka din möjlighet att anmäla dig till tävlingar.<br>Kontakta styrelsen om du har frågor.
+        .
       </h3>
     </div>
     <div v-if="user && user.granted_by == 0">
