5d4aa81ce3f22670c57c05c728120365a4c13680
Martin Sandström Krisell <25909128+Krisell@users.noreply.github.com>
2025-12-30 13:25:30 +0100

    Use Laravel Octane to speed up and simplify E2E test setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,24 @@
 import { devices } from '@playwright/test';
 import { defineConfig } from '@playwright/test';
 
+const SERVER_PORT = 65456;
+
 export default defineConfig({
   testDir: './tests/e2e',
   globalSetup: require.resolve('./tests/e2e/setup.mjs'),
+  webServer: {
+    command: `php artisan octane:start --workers=1 --port=${SERVER_PORT}`,
+    url: `http://localhost:${SERVER_PORT}`,
+    env: {
+      SESSION_SECURE_COOKIE: 'false', // Allow app to run on non-https
+      DB_CONNECTION: 'sqlite-e2e',
+      TELESCOPE_ENABLED: 'false',
+      DEBUGBAR_ENABLED: 'false',
+      MAIL_MAILER: 'array',
+      APP_ENV: 'testing',
+      BCRYPT_ROUNDS: '4',
+    }
+  },
   timeout: 30 * 1000,
   expect: {
     timeout: 5000
@@ -15,7 +30,7 @@ export default defineConfig({
   reporter: 'list',
   use: {
     actionTimeout: 0,
-    baseURL: 'http://localhost:65456',
+    baseURL: `http://localhost:${SERVER_PORT}`,
     trace: 'on-first-retry',
   },
   projects: [
--- tests/e2e/setup.mjs
+++ tests/e2e/setup.mjs
@@ -1,36 +1,11 @@
 import childProcess from 'child_process'
 
-const PORT = 65456
-
 function exec(cmd, env = {}) {
     // console.log('\x1b[36m%s\x1b[0m', `> ${cmd}`)
     childProcess.execSync(cmd, { stdio: 'inherit', env: { ...env, ...process.env } })
 }
 
 export default async function globalSetup() {
-    exec('php artisan route:cache', { APP_ENV: 'testing' })
     exec('touch database/e2e.sqlite')
     exec('php artisan migrate:fresh --database=sqlite-e2e')
-    exec(`php artisan serve --port=${PORT} > /dev/null &`, {
-        SESSION_SECURE_COOKIE: false, // Allow app to run on non-https
-        DB_CONNECTION: 'sqlite-e2e',
-        TELESCOPE_ENABLED: false,
-        DEBUGBAR_ENABLED: false,
-        MAIL_MAILER: 'array',
-        APP_ENV: 'testing',
-        BCRYPT_ROUNDS: 4,
-    })
-
-    return () => {
-        exec('php artisan route:clear')
-
-        // Kill any already running server on specified port
-        exec(`ALREADY_RUNNING_SERVERS=$(lsof -t -i:${PORT})
-echo $ALREADY_RUNNING_SERVERS
-if [ ! -z "$ALREADY_RUNNING_SERVERS" ]
-then
-    echo "Killing server"
-    kill "$ALREADY_RUNNING_SERVERS"
-fi`)
-    }
 }
