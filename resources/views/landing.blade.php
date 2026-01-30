87f8ac11bd7e2c2dd77bc2a68ab4a8dfe3703a08
Martin Sandström Krisell <25909128+Krisell@users.noreply.github.com>
2026-01-30 20:10:35 +0100

    Add documents for the annual meeting

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -90,6 +90,13 @@
                             <i class="fa fa-external-link ml-2 text-sm"></i>
                           </a>
                         </div>
+                        <div>
+                          <a href="https://firebasestorage.googleapis.com/v0/b/goteborg-kraftsportklubb.appspot.com/o/uploaded%2FpB9uYlSoXO7hip1x0BeC2saMChsyTm.pdf?alt=media&token=4e4cbe69-d45f-459c-bd16-617e600c8392" target="_blank" class="inline-flex items-center bg-white/10 hover:bg-white/20 text-white px-4 py-2 rounded-lg transition-all duration-200 hover:translate-x-1">
+                            <i class="fa fa-file-pdf-o mr-2"></i>
+                            Valberedningens förslag 2026
+                            <i class="fa fa-external-link ml-2 text-sm"></i>
+                          </a>
+                        </div>
                       </div>
                     </div>
                   </div>
