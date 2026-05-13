239f37f64c1dee7b58cf8ae5e2456c359d839628
Martin Sandström Krisell <25909128+Krisell@users.noreply.github.com>
2026-05-13 19:57:31 +0200

    feat: show confirmation message after sending password reset email

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -11,6 +11,23 @@
     <form action="{{ route('password.email') }}" method="POST">
         @csrf
 
+        @if (session('status'))
+        <div class="rounded-md bg-green-50 p-4 mb-4">
+            <div class="flex">
+                <div class="flex-shrink-0">
+                    <svg class="h-5 w-5 text-green-400" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                    </svg>
+                </div>
+                <div class="ml-3">
+                    <h3 class="text-sm leading-5 font-medium text-green-800">
+                        {{ session('status') }}
+                    </h3>
+                </div>
+            </div>
+        </div>
+        @endif
+
         <div class="rounded-md shadow-sm">
           <div>
             <input aria-label="Epost" name="email" type="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5" placeholder="Epost">
